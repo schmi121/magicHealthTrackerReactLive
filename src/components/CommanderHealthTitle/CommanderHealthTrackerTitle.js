2342aa2376c2152aa7026e89451865b37c1396a4
Jordan Schmidt <57489956+schmi121@users.noreply.github.com>
2020-02-09 10:39:25 -0800

    Titling Change

--- src/components/CommanderHealthTitle/CommanderHealthTrackerTitle.js
+++ src/components/CommanderHealthTitle/CommanderHealthTrackerTitle.js
@@ -4,6 +4,9 @@ import styles from './styles.module.css';
 const CommanderHealthTrackerTitle = () => {
     return <div className={styles.commanderHealthTracker}>
         <h1>Escape Plan's Commander Health Tracker</h1>
+        <div className={styles.accountability}>
+            <p>Keeping your kitchen table games accountable since 2020</p>
+        </div>
     </div>;
 };
 
